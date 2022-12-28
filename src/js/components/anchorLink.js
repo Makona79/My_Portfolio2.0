44d8c2c21b193c45c4aeb7d8845e8d8e2bdcffc9
Evgenii Kalukov <80322470+Makona79@users.noreply.github.com>
2022-12-28 15:04:59 +0400

    e

--- src/js/components/anchorLink.js
+++ src/js/components/anchorLink.js
@@ -1,10 +1,11 @@
 
-
 const menuLinks = document.querySelectorAll('.nav__link[data-goto]');
+
 if (menuLinks.length > 0) {
   menuLinks.forEach(menuLinks => {
     menuLinks.addEventListener("click", (e) => {
-      const menuLink = e.target;
+      const menuLink = e.target.closest('.nav__link');
+
       if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
         const gotoBlock = document.querySelector(menuLink.dataset.goto);
         const gotoBlockValue = gotoBlock.getBoundingClientRect().top;
